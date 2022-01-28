eb5d23e9c3ae63d863845d87605248776bf94fd0
JemiShieh <jemishieh@hotmail.com>
2022-01-28 03:40:21 -0500

    Updating MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,12 @@ lm(mpg ~  vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + A
 
 # Generate summary statistics
 summary(lm(mpg ~  vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_data)) 
+
+# Import and read csv file as table
+suspension_coil_data <- read.csv('Suspension_Coil.csv') 
+
+# Create total_summary dataframe using summarize() function to get the mean, median, variance, and standard deviation of the suspension coil's PSI column
+total_summary = summarise(suspension_coil_data, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Create lot_summary dataframe using group_by() and summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column
+lot_summary = suspension_coil_data %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
